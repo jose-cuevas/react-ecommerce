e3aebb38a455f31c5e115bf682695184cf16963e
jose-cuevas <jgutifer@gmail.com>
2022-08-10 13:36:06 +0200

    product card styles changed

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,41 +1,47 @@
 import { Link } from "react-router-dom";
 import products from "../../data/products";
-import { BsPlusLg, BsDashLg, BsTrash } from "react-icons/bs";
-
+import {
+  BsPlusLg,
+  BsDashLg,
+  BsFillCartFill,
+  BsFillHeartFill,
+  BsInfoCircleFill,
+} from "react-icons/bs";
 
 import "./productCard.css";
 import "../Products/products.css";
 
 function ProductCard({ product, onAdd, addWishList }) {
   return (
-    <>   
-      
-        <article className="card m-2">
-          <img src={product.img} alt={product.img} className="img-thumbnail" />
-          <div>{product.title}</div>
-          <div>{product.price} €</div>
+    <>
+      <article className="card m-2 ">
+        <img src={product.img} alt={product.img} className="img-thumbnail" />
+        <div className="ps-3 mt-2">{product.title}</div>
+        <div className="ps-3 mb-2">{product.price} €</div>
+        <section className="d-flex justify-content-center">
           <Link to={`/products/${product.id}`}>
-            <button className="btn btn-secondary btn-sm counter-button mb-1 container-fluid">
-              More info
+            <button className="btn  btn-sm counter-button mb-1 ">
+              <BsInfoCircleFill
+                style={{ fontSize: "1.2rem", color: "#6c757d" }}
+              />
             </button>
           </Link>
           <button
             type="button"
-            className="btn btn-secondary btn-sm counter-button mb-1"
+            className="btn  btn-sm counter-button mb-1"
             onClick={() => onAdd(product)}
           >
-            Add to basket
+            <BsFillCartFill style={{ fontSize: "1.2rem", color: "#6c757d" }} />
           </button>
           <button
             type="button"
-            className="btn btn-primary btn-sm"
+            className="btn  btn-sm"
             onClick={() => addWishList(product)}
           >
-            Add to whishlist
+            <BsFillHeartFill style={{ fontSize: "1.1rem", color: "#6c757d" }} />
           </button>
-        </article>
-       
-       
+        </section>
+      </article>
     </>
   );
 }
